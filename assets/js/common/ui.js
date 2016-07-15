3772ebd78244a776908146ddf092bc9d70fc1382
otamasayuki <otamasayuki@otamasayuki-no-MacBook-Pro.local>
2016-07-15 16:10:52 +0900

    To Page Top 追加

--- assets/js/common/ui.js
+++ assets/js/common/ui.js
@@ -9,7 +9,6 @@ module.exports = function() {
 
   // スクロールでカテゴリーヘッダーを表示
   let drowerCategoryMenu = {
-
     setEvent : function() {
       let self = this;
       $window.on('scroll', function(){
@@ -28,19 +27,38 @@ module.exports = function() {
     },
 
     show : function($el) {
-      $el.animate({
-        top: '0'
-      });
+      $el.animate({ top: '0' });
     },
 
     hide : function($el) {
-      $el.animate({
-        top: '-100'
-      });
-    },
+      $el.animate({ top: '-100' });
+    }
+  };
 
+  // ページTOPへ
+  let setToPageTopEvent = () => {
+    let $toPageTopBtn = $('#js-to-page-top');
+
+    $toPageTopBtn.hide();
+
+    $window.on('scroll', function(){
+      if ($(this).scrollTop() > 500) {
+        $toPageTopBtn.fadeIn();
+      } else {
+        $toPageTopBtn.fadeOut();
+      }
+    });
+
+    $toPageTopBtn.on('click', function(e){
+      e.preventDefault();
+      //TOPまでスクロール
+      $('body,html').animate({
+        scrollTop: 0
+      }, 500);
+    });
   };
 
   drowerCategoryMenu.setEvent();
+  setToPageTopEvent();
 
 };
